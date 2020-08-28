504cddbfcbd2e7c7a2b280ebdc7242472975fee7
EdwinMLara <emlara35@gmail.com>
2020-08-27 21:16:58 -0500

    Terminado

--- js/game.js
+++ js/game.js
@@ -84,9 +84,8 @@ function checkClick(event){
 }
 
 canvas.onclick = (e) =>{
-    console.log(e);
     let elementClikedId = checkClick(e);
-    console.log(`se selecciono el cuadro ${elementClikedId}`);
+    //console.log(`se selecciono el cuadro ${elementClikedId}`);
     tiles_array[elementClikedId].fillcolor = 'black';
     drawTiles();
 }
@@ -99,63 +98,80 @@ function array2Matriz(array,col){
     return res;
 }
 
- var matriz_tiles = array2Matriz(tiles_array,numCellX);
+var matriz_tiles = array2Matriz(tiles_array,numCellX);
 
- function gameoflife(){
-     var i1,k1,aux1,aux2,stateCell;
-     var numCell = 0;
-     let neighbours = 0;
-     for(var i = 1;i<numCellY-1;i++){
-         for(var k = 1;k<numCellX-1;k++){
-            i1 = 0;
-            k1 = 0;
-            if(matriz_tiles[i][k] === 'black'){
-                stateCell = 'alive';
-                numCell += 1;
-            }else{
-                stateCell = 'dead';
-            }
-            for (aux1 = i-1;aux1<=i+1;aux1++){
-                for(aux2 = k-1; aux2<=k+1;aux2++){
-                    if(matriz_tiles[aux1][aux2] === 'black'){
-                        neighbours += 1;
-                    }
+function gameoflife(){
+    var arrayLiveCell = [];
+    var arrayDeadCell = [];
+    var i1,k1,aux1,aux2,stateCell;
+    var numCell = 0;
+    let neighbours = 0;
+    for(var i = 1 ; i<numCellY-1 ;i ++){
+        for(var k = 1; k<numCellX-1 ; k++){
+        i1 = 0;
+        k1 = 0;
+
+        for (aux1 = i-1; aux1 <= i+1; aux1++){
+            for(aux2 = k-1; aux2 <= k+1 ; aux2++){ 
+                if(matriz_tiles[aux1][aux2].fillcolor === 'black'){
+                    neighbours += 1;
                 }
+                k1++;
             }
-            
-            if(neighbours){
-                neighbours -= 1;
-            }
+            k1=0;
+            i1++;
+        }
 
-            switch(neighbours){
-                case 2:
-                    matriz_tiles[i][k].fillcolor = 'black';
-                    break;
-                case 3:
-                    if(stateCell === 'dead'){
-                        matriz_tiles[i][k].fillcolor = 'black';
-                    }
-                    break;
-                default:
-                    matriz_tiles[i][k] = '#C7FF33';
+        if(matriz_tiles[i][k].fillcolor === 'black'){
+            stateCell = 'alive';
+            numCell += 1;
+            neighbours -= 1;
+        }else{
+            stateCell = 'dead';
+        }
 
-            }
-            neighbours = 0;
-         }
-     }
-     
-     if(numCell){
-        drawTiles();
-     }else{
-        alert('No hay celulas vivas, active algunas');
-     }
- }
+        switch(neighbours){
+            case 2:
+                if(stateCell === 'alive'){
+                    arrayLiveCell.push(matriz_tiles[i][k].id);
+                }
+                break;
+            case 3:
+                if(stateCell === 'dead'){
+                    arrayLiveCell.push(matriz_tiles[i][k].id);
+                }
+                break;
+            default:
+                if(stateCell === 'alive'){
+                    arrayDeadCell.push(matriz_tiles[i][k].id);
+                }
+        }
+        neighbours = 0;
+        }
+    }
+    if(numCell){
+    arrayLiveCell.forEach((currentValue) => {
+        tiles_array[currentValue].fillcolor = 'black';
+    })
+    arrayDeadCell.forEach((currentValue) => {
+        tiles_array[currentValue].fillcolor = '#C7FF33'; 
+    })
+    drawTiles();
+    }else{
+    alert('No hay celulas vivas, active algunas');
+    }
+}
 
+var iniciar = document.getElementById("iniciar");
+var detener = document.getElementById("detener");
+var interval;
+iniciar.onclick = () =>{
+    interval = setInterval(gameoflife,500);
+}
 
- var iniciar = document.getElementById("iniciar");
- iniciar.onclick = () =>{
-     gameoflife();
- }
+detener.onclick = () =>{
+    clearInterval(interval);
+}
 
 
 
