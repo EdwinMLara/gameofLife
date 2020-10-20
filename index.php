2f84485aa2e09962036841d85b7304449387d954
EdwinMLara <emlara35@gmail.com>
2020-10-19 23:52:05 -0500

    Agregando radio buttons para seleccionar entre ejercicios

--- index.php
+++ index.php
@@ -7,11 +7,16 @@
 </head>
 <body>
     <div><canvas id="canvas" width="500" height="200" style="border:1px solid #000000;"></canvas></div>
+    <div id="examples">
+        <input type="radio" id="r1" name="rate" value="1" checked="checked" onclick="handleRadios(this)"> Game of Life
+        <input type="radio" id="r2" name="rate" value="2" onclick="handleRadios(this)"> A*
+        <input type="radio" id="r3" name="rate" value="3" onclick="handleRadios(this)"> Arboles
+    </div>
     <div><button id="iniciar">Iniciar</button><button id="detener">Detener</button>
     <button id="guardar">Guardar Patron</button><input type="file" id="cargar"/></div>
 </body>
 <script src="js/variables.js"></script>
 <script src="js/pintado.js"></script>
 <script src="js/gameOfLife.js"></script>
-<script src="js/eventos"></script> 
+<script src="js/eventos.js"></script> 
 </html>
