0830b69f04e66ea6ef945b8e136fdb9a45f93c6e
EdwinMLara <emlara35@gmail.com>
2021-01-24 22:59:37 -0600

    Agregando a* algorithm

--- js/aStart.js
+++ js/aStart.js
@@ -1,12 +1,6 @@
-var openList = [];
-var closeList = [];
 var nodeArray = [];
-var startNode = matriz_tiles[0][0];
-var endNode = matriz_tiles[numCellY-1 ][numCellX-1]; 
-openList.push(startNode)
-
-function Node(x,y,width,height,fillcolor){
-    Tile.call(x,y,width,height,fillcolor);
+function Node(x,y,width,height,id,fillcolor){
+    Tile.call(this,x,y,width,height,id,fillcolor);
     this.f = 0;
     this.g = 0;
     this.h = 0;
@@ -24,8 +18,8 @@ function createTilesNodes(quantityx,quantityy){
     }
 
     for(let i=0;i<quantityAll;i++){
-        let fillcolor = '#C7FF33';
-        let node = new Node(drawPositionNode.x,drawPositionNode.y,tile_width,tile_height,i,fillcolor);
+        let fillcolor2 = '#C7FF33';
+        let node = new Node(drawPositionNode.x,drawPositionNode.y,tile_width,tile_height,i,fillcolor2);
         nodeArray.push(node);
         drawPositionNode.x = drawPositionNode.x + tile_width;
 
@@ -34,4 +28,18 @@ function createTilesNodes(quantityx,quantityy){
             drawPositionNode.y = drawPositionNode.y + tile_height;
         }
     }
+}
+
+createTilesNodes(numCellX,numCellY);
+console.log(nodeArray);
+//drawTiles(nodeArray);
+matrixNodes = array2Matriz(nodeArray,numCellX);
+
+function aStar(){
+    var openList = [];
+    var closeList = [];
+    var startNode = matrixNodes[0][0];
+    var endNode = matrizNodes[numCellY-1][numCellX-1];
+    openList.push(startNode) 
+
 }
